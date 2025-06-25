87580d55053e9ed57faed61edb66fcd2fa080314
prxnav2005 <pranavmofficial2005@gmail.com>
2025-06-25 12:00:43 +0530

    feat: add top module

---
+++ src/top.sv
@@ -0,0 +1,8 @@
+module top(input logic clk, reset, output logic [31:0] WriteDataM, DataAdrM, output logic MemWriteM);
+  
+  logic [31:0] PCF, InstrF, ReadDataM;
+  
+  riscv riscv(clk, reset, PCF, InstrF, MemWriteM, DataAdrM, WriteDataM, ReadDataM);
+  imem imem(PCF, InstrF);
+  dmem dmem(clk, MemWriteM, DataAdrM, WriteDataM, ReadDataM);
+endmodule
